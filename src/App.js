c4e2ff9dfaff433ac5048566dce31709a55dbc94
Pavlo <pavlo.malishenko@gmail.com>
2024-12-01 22:04:27 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    if (window.Telegram) {
+      const tg = window.Telegram.WebApp;
+      tg.ready(); // Notify Telegram that the app is ready
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a Telegram Mini App powered by React.</p>
     </div>
   );
-}
+};
 
 export default App;
